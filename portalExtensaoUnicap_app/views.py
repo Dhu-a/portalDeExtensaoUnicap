5af2ee774790a0adcfd43c38bc79a15611404c1f
GabrielaRBLemos <gabrielarblemos@gmail.com>
2025-04-01 11:39:10 -0300

    nav styling

--- portalExtensaoUnicap_app/views.py
+++ portalExtensaoUnicap_app/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 
 def PortalExtensaoView(request):
-    return render(request, 'base.html')
+    return render(request, 'admin-cards.html')
 
 def AdminPageView(request):
-    return render(request, 'admin-base.html')
+    return render(request, 'admin-cards.html')
