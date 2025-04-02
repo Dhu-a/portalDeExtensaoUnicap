ae3376eee5340bbdd88ab80f3494312299c6e19b
VictorHugoJSS <victor.-hugo@outlook.com.br>
2025-04-01 21:09:11 -0300

    loginView Feito

--- portalExtensaoUnicap_app/views.py
+++ portalExtensaoUnicap_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
 # Create your views here.
 
 def PortalExtensaoView(request):
@@ -12,5 +13,16 @@ def Create(request):
     return render(request, 'create.html')
 
 def LoginView(request):
+    if request.method == "POST":
+        email = request.post['email']
+        password = request.post['password']
+        user = authenticate(request=request, email=email, password=password)
+        
+        if user is not None:
+            login(request, user)
+            return redirect('admin')
+        else:
+            messages.error(request, 'E-mail ou Senha incorreto')            
+            redirect('login')
     return render(request, 'login.html')
 
