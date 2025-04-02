8777e050635551e94f5caee5a7f003a8702546f5
Roberto Regis <robertoregisaraujoneto@gmail.com>
2025-04-01 21:41:53 -0300

    Esqueci o from .models import projeto

--- portalExtensaoUnicap_app/views.py
+++ portalExtensaoUnicap_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth import login, authenticate
 from django.contrib import messages
+from .models import projeto
 
 # Create your views here.
 
