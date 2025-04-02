7fb194d8aa0e90e4ee0c4bbae4c4aba8b6be62a1
JuliaSilva05 <jusilva.souto@gmail.com>
2025-04-02 07:39:52 -0300

    registrando modelos (esqueci uma coisa)

--- portalExtensaoUnicap_app/admin.py
+++ portalExtensaoUnicap_app/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import projeto, dias, areas, adm;
+from portalExtensaoUnicap_app.models import projeto, dias, areas, adm;
 
 # Register your models here.
 admin.site.register(projeto)
